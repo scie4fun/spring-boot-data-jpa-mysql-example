c713e5a4d74a33910f6f83f23eee72fec9b4c6b8
scie4fun <scie4fun@gmail.com>
2017-08-22 13:46:44 +0200

    FrontEnd part added single user view, optimized controllers

--- src/main/java/org/spring/boot/data/jpa/mysql/example/controllers/HomeController.java
+++ src/main/java/org/spring/boot/data/jpa/mysql/example/controllers/HomeController.java
@@ -5,11 +5,13 @@ import org.spring.boot.data.jpa.mysql.example.repository.UserRepository;
 import org.spring.boot.data.jpa.mysql.example.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class HomeController {
 
     private final UserService userService;
@@ -20,14 +22,22 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public List<User> home() {
-        return userService.getAll();
+    public String home() {
+        return "redirect:/users";
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping("/users")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("users");
+        modelAndView.addObject("users", userService.getAll());
+        return modelAndView;
+    }
+
+    @RequestMapping(value = {"/{id}", "/users/{id}"}, method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public User homeId(@PathVariable("id") Long id) {
-        return userService.getOne(id);
+    public ModelAndView homeId(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject("user", userService.getOne(id));
+        return modelAndView;
     }
 }
